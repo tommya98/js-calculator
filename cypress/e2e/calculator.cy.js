74e71b3a72d7d43f609edf5e108123b48d5846db
tommya98 <tommya98@naver.com>
2024-01-01 14:45:00 +0900

    FIX: 디스플레이 테스트 문자열 사용으로 수정

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -14,21 +14,21 @@ describe("계산기 앱 테스트", () => {
   });
 
   it("디스플레이에 기본적으로 숫자 0이 표시된다.", () => {
-    checkDisplayValue(0);
+    checkDisplayValue("0");
   });
 
   it("1개의 숫자 버튼을 클릭하면 display에 숫자가 표시된다.", () => {
     clickDigitButtons([1]);
-    checkDisplayValue(1);
+    checkDisplayValue("1");
   });
 
   it("2개의 숫자 버튼을 클릭하면 display에 숫자가 표시된다.", () => {
     clickDigitButtons([1, 2]);
-    checkDisplayValue(12);
+    checkDisplayValue("12");
   });
 
   it("3개의 숫자 버튼을 클릭하면 display에 숫자가 표시된다.", () => {
     clickDigitButtons([1, 2, 3]);
-    checkDisplayValue(123);
+    checkDisplayValue("123");
   });
 });
