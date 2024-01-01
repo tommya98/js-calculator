f597c40dd1941feff118deac8a258b3a68c3e366
tommya98 <tommya98@naver.com>
2024-01-01 14:27:08 +0900

    TEST: 클릭한 숫자 디스플레이 테스트 작성

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -6,4 +6,9 @@ describe("계산기 앱 테스트", () => {
   it("디스플레이에 기본적으로 숫자 0이 표시된다.", () => {
     cy.get("#total").should("have.text", 0);
   });
+
+  it("숫자 버튼을 클릭하면 display에 숫자가 표시된다.", () => {
+    cy.get(".digit").contains(1).click();
+    cy.get("#total").should("have.text", 1);
+  });
 });
