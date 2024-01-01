77567f342285aa70f819073dfd904fd566c662e2
tommya98 <tommya98@naver.com>
2024-01-01 14:40:30 +0900

    REFACTOR: 테스트 코드 함수 분리 리팩토링

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -1,27 +1,34 @@
+const clickDigitButtons = (digits = []) => {
+  digits.forEach((digit) => {
+    cy.get(".digit").contains(digit).click();
+  });
+};
+
+const checkDisplayValue = (value) => {
+  cy.get("#total").should("have.text", value);
+};
+
 describe("계산기 앱 테스트", () => {
   beforeEach("페이지 방문", () => {
     cy.visit("../../index.html");
   });
 
   it("디스플레이에 기본적으로 숫자 0이 표시된다.", () => {
-    cy.get("#total").should("have.text", 0);
+    checkDisplayValue(0);
   });
 
   it("1개의 숫자 버튼을 클릭하면 display에 숫자가 표시된다.", () => {
-    cy.get(".digit").contains(1).click();
-    cy.get("#total").should("have.text", 1);
+    clickDigitButtons([1]);
+    checkDisplayValue(1);
   });
 
   it("2개의 숫자 버튼을 클릭하면 display에 숫자가 표시된다.", () => {
-    cy.get(".digit").contains(1).click();
-    cy.get(".digit").contains(2).click();
-    cy.get("#total").should("have.text", 12);
+    clickDigitButtons([1, 2]);
+    checkDisplayValue(12);
   });
 
   it("3개의 숫자 버튼을 클릭하면 display에 숫자가 표시된다.", () => {
-    cy.get(".digit").contains(1).click();
-    cy.get(".digit").contains(2).click();
-    cy.get(".digit").contains(3).click();
-    cy.get("#total").should("have.text", 123);
+    clickDigitButtons([1, 2, 3]);
+    checkDisplayValue(123);
   });
 });
