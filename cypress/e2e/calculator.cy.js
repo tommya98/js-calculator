5d70df86f941868c9ed46e56baeb4700da89abe7
tommya98 <tommya98@naver.com>
2024-01-01 14:55:16 +0900

    TEST: 계산기 기본 동작 테스트 작성

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -4,8 +4,24 @@ const clickDigitButtons = (digits = []) => {
   });
 };
 
+const clickOperationButton = (operation) => {
+  cy.get(".operation").contains(operation).click();
+};
+
+const clickACButton = () => {
+  cy.get(".modifier").click();
+};
+
 const checkDisplayValue = (value) => {
   cy.get("#total").should("have.text", value);
+};
+
+const calculate = (first, operation, second, result) => {
+  clickDigitButtons(first);
+  clickOperationButton(operation);
+  clickDigitButtons(second);
+  clickOperationButton("=");
+  checkDisplayValue(result);
 };
 
 describe("계산기 앱 테스트", () => {
@@ -30,5 +46,47 @@ describe("계산기 앱 테스트", () => {
   it("3개의 숫자 버튼을 클릭하면 display에 숫자가 표시된다.", () => {
     clickDigitButtons([1, 2, 3]);
     checkDisplayValue("123");
+  });
+
+  xit("1번째 숫자를 입력하고 연산자 버튼을 누르면 display에 연산자가 표시된다.", () => {
+    clickDigitButtons([1, 2, 3]);
+    clickOperationButton("+");
+    checkDisplayValue("123+");
+  });
+
+  xit("연산자 버튼을 여러 번 누르면 display에 마지막 연산자만 표시된다.", () => {
+    clickDigitButtons([1, 2, 3]);
+    clickOperationButton("+");
+    clickOperationButton("-");
+    checkDisplayValue("123-");
+  });
+
+  xit("2번째 숫자를 입력하면 display에 2번째 숫자를 누적하여 표시한다.", () => {
+    clickDigitButtons([1, 2, 3]);
+    clickOperationButton("+");
+    clickDigitButtons([4, 5, 6]);
+    checkDisplayValue("123+456");
+  });
+
+  xit("덧셈: 두 숫자를 입력하고 =버튼을 클릭하면 display에 연산의 결과값이 표시된다.", () => {
+    calculate([1, 2, 3], "+", [4, 5, 6], "579");
+  });
+
+  xit("뺄셈: 두 숫자를 입력하고 =버튼을 클릭하면 display에 연산의 결과값이 표시된다.", () => {
+    calculate([1, 2, 3], "-", [4, 5, 6], "-333");
+  });
+
+  xit("곱셈: 두 숫자를 입력하고 =버튼을 클릭하면 display에 연산의 결과값이 표시된다.", () => {
+    calculate([1, 2, 3], "X", [4, 5, 6], "56088");
+  });
+
+  xit("나눗셈: 두 숫자를 입력하고 =버튼을 클릭하면 display에 연산의 결과값이 표시된다.", () => {
+    calculate([1, 2, 3], "/", [4, 5, 6], "0");
+  });
+
+  xit("AC버튼을 클릭하면 display에 0이 표시된다.", () => {
+    clickDigitButtons([1, 2, 3]);
+    clickACButton();
+    checkDisplayValue("0");
   });
 });
