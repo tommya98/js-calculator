f9ea43ba1ddb2201de047cef914fbaaf29d1e103
tommya98 <tommya98@naver.com>
2024-01-01 14:21:49 +0900

    TEST: 계산기 0 디스플레이 테스트 작성

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -1,5 +1,9 @@
 describe("계산기 앱 테스트", () => {
-  it("계산기 페이지 접속", () => {
+  beforeEach("페이지 방문", () => {
     cy.visit("../../index.html");
+  });
+
+  it("디스플레이에 기본적으로 숫자 0이 표시된다.", () => {
+    cy.get("#total").should("have.text", 0);
   });
 });
